027b17146c35794b01f39408ce956f4a63f055ff
Pratik Darak <darak.pratik@gmail.com>
2020-05-13 15:39:23 +0530

    implemented firebase utility to store authenticated users into our firestore database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -36,7 +36,7 @@ class SignIn extends React.Component {
             type="email"
             value={this.state.email}
             handleChange={this.handleChange}
-            label="email"
+            label="Email"
             required
           />
 
@@ -45,7 +45,7 @@ class SignIn extends React.Component {
             type="password"
             value={this.state.password}
             handleChange={this.handleChange}
-            label="password"
+            label="Password"
             required
           />
           <div className="buttons">
