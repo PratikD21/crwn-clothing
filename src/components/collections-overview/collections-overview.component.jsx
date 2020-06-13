f904d672ad8df0466afccf4ebe81f8a049bc1343
Pratik Darak <darak.pratik@gmail.com>
2020-06-13 16:57:16 +0530

    fiexed shop link click issue

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import "./collections-overview.styles.scss";
 import CollectionPreview from "../collection-preview/collection-preview.component";
+
 import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+
+import "./collections-overview.styles.scss";
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
